a10f456827544176a57f5d7287d8087bd951c3f5
Biljana Temovska <63975308+btemovska@users.noreply.github.com>
2020-05-11 20:09:18 -0400

    Update DataMiningModeling2.R

--- DataMiningModeling2.R
+++ DataMiningModeling2.R
@@ -8,9 +8,9 @@ library(party)
 library("RSQLite")
 
 #Prepare Data
-setwd("~/SQLite") #set working directory
-db<-dbConnect(SQLite(), dbname="DataMining.db") #establish connection with DB
-dbListTables(db) # inventory_sales is the table
+setwd("~/SQLite")
+db<-dbConnect(SQLite(), dbname="DataMining.db")
+dbListTables(db) 
 df<-data.frame(dbGetQuery(db, 'select * from inventory_sales')) #[1] 942 rows 7 variables
 str(df)
 
@@ -18,16 +18,16 @@ str(df)
 df<-select(df, Sale, Stock, Season, StockLevel) #[1] 942 rows 4 variables
 head(df)
 str(df)
-df<-mutate(df, StockLevel=factor(StockLevel))  #creating the factor
+df<-mutate(df, StockLevel=factor(StockLevel))  
 str(df)
 
 #Split data into training and test data sets
 set.seed(1234)
-g<-runif(nrow(df)) #randomise my data as it was in ascending order by Date
-df<-df[order(g),] #now reorder it
+g<-runif(nrow(df)) 
+df<-df[order(g),]
 head(df)
 
-sample=sample.split(df$StockLevel, SplitRatio = 0.70) #70/30 split gave me more accurate model, then 80/20
+sample=sample.split(df$StockLevel, SplitRatio = 0.70)
 train=subset(df, sample==TRUE)
 test=subset(df, sample==FALSE)
 
@@ -35,7 +35,7 @@ test=subset(df, sample==FALSE)
 T1<-rpart(StockLevel~ ., data=train)
 T1
 prp(T1, main= "Tree_Train")
-summary(T1) #variable importance shows Sale was always first, Stock was second, and Seasonality was last
+summary(T1) 
 #Prediction with Train
 P1<-predict(T1, train, type = 'class')
 #Confusion Matrix with Train
